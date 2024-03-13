f47e2fcb0e3fbf42df414788a01ad69f18ad9411
doubleBeefStew <30426390+doubleBeefStew@users.noreply.github.com>
2024-03-13 10:12:49 +0000

    modified:   frontend/ecommerce-frontend/src/App.jsx

--- frontend/ecommerce-frontend/src/App.jsx
+++ frontend/ecommerce-frontend/src/App.jsx
@@ -2,28 +2,28 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const App = ()=>{
-    const [msg, SetMsg] = useState('')
+    const [message, SetMessage] = useState('')
 
     useEffect(()=>{
-        SetMsg('loading..')
+        SetMessage('loading..')
         axios({
             url:'https://studious-couscous-x9gxvg999g2v5wx-3000.app.github.dev/api'
         })
             .then((res)=>{
-                console.log(res.data.msg)
-                SetMsg(res.data.msg)
+                console.log(res.data.message)
+                SetMessage(res.data.message)
 
             })
             .catch((err)=>{
                 console.log(err.message)
-                SetMsg(err.message)
+                SetMessage(err.message)
             })
     },[])
 
     return (
         <>
             <h1>test application</h1>
-            <h4>{msg ? msg : 'loading..'}</h4>
+            <h4>{message ? message : 'loading..'}</h4>
         </>
     )
 }
