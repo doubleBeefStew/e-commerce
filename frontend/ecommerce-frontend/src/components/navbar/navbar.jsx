da62c50e2e1d2b0b7249df00ad509f7a52f16e37
doubleBeefStew <30426390+doubleBeefStew@users.noreply.github.com>
2024-03-23 19:02:34 +0000

    finished navbar

--- frontend/ecommerce-frontend/src/components/navbar/navbar.jsx
+++ frontend/ecommerce-frontend/src/components/navbar/navbar.jsx
@@ -19,29 +19,31 @@ const AppNav = ()=>{
                 </span>
             </Col>
         </Row>
-        <Row className='navbar py-0'>
-            <Col className='col-2 ps-sm-3 pe-sm-5 d-xs-none d-flex align-items-center justify-content-center'>
-                <Logo className='d-none'/>
+        <Row className='navbar'>
+            <div className='col-auto'>
+                <Logo className='d-none' />
+            </div>
+            <Col>
+                <Row>
+                    <Col className='p-0 d-flex flex-column justify-content-center'>
+                        <div className={`${styles.searchBar} mb-1 shadow-sm`}>
+                            <Form className="d-flex">
+                                <Form.Control className="border-0" type="text" name="search" placeholder="Daftar & Dapatkan Voucher Gratis"/>
+                                    <Button className={`${styles.searchButton} btn btn-primary border-0`}><BsSearch/></Button>
+                            </Form>
+                        </div>
+                        <div className="d-flex justify-content-start align-items-center">
+                                <Link className="px-1 text-decoration-none text-white" to={'/home'}>Home</Link>
+                                <Link className="px-1 text-decoration-none text-white" to={'/home'}>Home</Link>
+                                <Link className="px-1 text-decoration-none text-white" to={'/home'}>Home</Link>
+                                <Link className="px-1 text-decoration-none text-white" to={'/home'}>Home</Link>
+                        </div>
+                    </Col>
+                </Row>
             </Col>
-            <Col className='col-8 p-0 d-flex flex-column justify-content-center'>
-                <div className={`${styles.searchBar} mb-1 shadow-sm`}>
-                    <Form className="d-flex">
-                        <Form.Control className="border-0" type="text" name="search" placeholder="Daftar & Dapatkan Voucher Gratis"/>
-                            <Button className={`${styles.searchButton} btn btn-primary border-0`}><BsSearch/></Button>
-                    </Form>
-                </div>
-                <div className="d-flex justify-content-start align-items-center">
-                        <Link className="px-1 text-decoration-none text-white" to={'/home'}>Home</Link>
-                        <Link className="px-1 text-decoration-none text-white" to={'/home'}>Home</Link>
-                        <Link className="px-1 text-decoration-none text-white" to={'/home'}>Home</Link>
-                        <Link className="px-1 text-decoration-none text-white" to={'/home'}>Home</Link>
-                </div>
-            </Col>
-            <Col className='col-2 p-0 d-flex align-items-center justify-content-center text-center'>
-                <div>
-                    <BsCart3 color='white' size='30'/>
-                </div>
-            </Col>
+            <div className='col-auto px-4'>
+                <BsCart3 color='white' size='30'/>
+            </div>
         </Row>
     </>)
 }
