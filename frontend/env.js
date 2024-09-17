ba6580c33c5a3e147f94db61f536fdc638bd700c
Lucid Dreamer <145696672+manoflucid@users.noreply.github.com>
2024-09-17 16:29:10 +0000

    add vercel.json

--- frontend/env.js
+++ frontend/env.js
@@ -1,6 +1,7 @@
 const env = {
     // API_URL:'https://studious-couscous-x9gxvg999g2v5wx-3000.app.github.dev/api/v1'
-    API_URL:'https://obscure-spork-7v9jxqqwwr7xcxvjr-3000.app.github.dev/api/v1'
+    // API_URL:'https://obscure-spork-7v9jxqqwwr7xcxvjr-3000.app.github.dev/api/v1'
+    API_URL:'https://e-commerce-ten-alpha-54.vercel.app/api/v1'
 }
 
 export default env
