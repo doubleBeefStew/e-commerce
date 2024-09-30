8a49bd0bdcae8f59f6e6272aa7c69bd1916ae901
Lucid Dreamer <145696672+manoflucid@users.noreply.github.com>
2024-09-30 03:02:19 +0000

    fix no image products

--- frontend/ecommerce-frontend/src/components/Carousel/productCarousel.jsx
+++ frontend/ecommerce-frontend/src/components/Carousel/productCarousel.jsx
@@ -6,9 +6,13 @@ import styles from './productCarousel.module.css'
 const ProductCarousel = ({images}) => {
     const [imgLink,setImgLink] = useState(null)
     let index = 0
+    const noImage = '../../errors/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.avif'
 
     useEffect(()=>{
-        setImgLink(images[index].url)
+        if(images && images.length > 0)
+            setImgLink(images[index].url)
+        else
+            setImgLink(noImage)
     },[])
 
     return (
@@ -18,7 +22,7 @@ const ProductCarousel = ({images}) => {
             </div>
             <Row className='m-0 w-100 flex-nowrap overflow-scroll justify-content-start' style={{height:100}} >
                 {
-                    images.map((item)=>{
+                    images && images.length > 0 ? images.map((item)=>{
                         return (
                             <Col
                                 className={`col-3 p-0 me-1 ${styles.thumbnail}`}
@@ -29,7 +33,13 @@ const ProductCarousel = ({images}) => {
                                     src={item.url} onClick={()=>{setImgLink(item.url)}}/>
                             </Col>
                         )
-                    })
+                    }) :
+                        <Col
+                            className={`col-3 p-0 me-1 ${styles.thumbnail}`}style={{width:100,height:100}}>
+                            <img 
+                                className='w-100 h-100 object-fit-cover overflow-hidden'
+                                src={noImage} onClick={()=>{setImgLink(noImage)}}/>
+                        </Col>
                 }
             </Row>
         </div>
